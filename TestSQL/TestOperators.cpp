50e5d1faffe7de42dcd313c66004eb5df46882c7
Edwig Huisman <edwig.huisman@centric.eu>
2016-11-20 19:34:55 +0100

    Testing operators

--- TestSQL/TestOperators.cpp
+++ TestSQL/TestOperators.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "SQLVariant.h"
-
+#include "SQLDate.h"
 
 void TestGreater()
 {
@@ -13,8 +13,19 @@ void TestGreater()
   }
 }
 
+void TestSub()
+{
+  SQLDate date1("15-10-1959");
+  SQLDate date2("20-11-2016");
+  SQLVariant var1(&date1);
+  SQLVariant var2(&date2);
+
+  SQLVariant var3 = var2 - var1;
+  printf("Found interval is: %s\n",var3.GetAsChar());
+}
 
 void TestOperators()
 {
   TestGreater();
+  TestSub();
 }
